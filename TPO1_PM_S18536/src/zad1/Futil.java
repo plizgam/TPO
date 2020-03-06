492801a98148085d1d9a8927c37d84b6e7e6c8e2
unknown <milosz.plizga1@gmail.com>
2020-03-06 23:30:38 +0100

    working read, but problem with write files

--- TPO1_PM_S18536/src/zad1/Futil.java
+++ TPO1_PM_S18536/src/zad1/Futil.java
@@ -1,23 +1,57 @@
 package zad1;
 
 import java.io.IOException;
+import java.io.RandomAccessFile;
+import java.nio.ByteBuffer;
+import java.nio.channels.FileChannel;
+import java.nio.charset.Charset;
 import java.nio.file.*;
 import java.nio.file.attribute.BasicFileAttributes;
 
 
 public class Futil {
 
+    //plik do zapisu
+    public static Path path = Paths.get("TPO1res.txt");
+
     public static void processDir(String dirName, String resultFileName) {
         try {
             Files.walkFileTree(Paths.get(dirName), new SimpleFileVisitor<Path>() {
                 @Override
                 public FileVisitResult visitFile(Path file, BasicFileAttributes attrs) throws IOException {
-                    System.out.println("File visited " + file.toString());
+                    readFile(file.toString());
                     return super.visitFile(file, attrs);
                 }
             });
         }catch(Exception ex){
             ex.printStackTrace();
         }
+    }
+
+    public static void readFile(String path) throws IOException {
+
+        RandomAccessFile file = new RandomAccessFile(path, "r");
+        FileChannel channel = file.getChannel();
+        ByteBuffer byteBuffer = ByteBuffer.allocate(512);
+        Charset charset = Charset.forName("Cp1250");
+
+        while(channel.read(byteBuffer) > 0){
+            byteBuffer.rewind();
+            writeToFile(byteBuffer);
+            byteBuffer.flip();
+        }
+
+        channel.close();
+        file.close();
+
+    }
+
+
+    public static void writeToFile(ByteBuffer byteBuffer) throws IOException {
+
+        FileChannel channel = FileChannel.open(path, StandardOpenOption.CREATE, StandardOpenOption.APPEND);
+        channel.write(byteBuffer);
+        channel.close();
+
     }
 }
