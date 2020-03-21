7f073730ba7d1aeda3a4cf9b497630572f045890
Miłosz Pliżga <milosz.plizga1@gmail.com>
2020-03-21 14:59:01 +0100

    Init 1

---
+++ TPO2_PM_S18536/src/zad1/Service.java
@@ -0,0 +1,28 @@
+/**
+ *
+ *  @author Pliżga Miłosz S18536
+ *
+ */
+
+package zad1;
+
+    OpenWeather
+public class Service {
+
+    public Service(String country){
+
+    }
+
+    String getWeather(String city){
+
+    }
+
+    Double getRateFor(String currency){
+
+    }
+
+    Double getNBPRate(){
+
+    }
+
+}  
