08ccafaea666b4ad76069f5e0507efb1e1574d53
Miłosz Pliżga <milosz.plizga1@gmail.com>
2020-04-04 16:46:51 +0200

    first check by SDKP

---
+++ TPO3_PM_S18536/src/zad1/Tools.java
@@ -0,0 +1,31 @@
+/**
+ *
+ *  @author Pliżga Miłosz S18536
+ *
+ */
+
+package zad1;
+
+
+import org.yaml.snakeyaml.Yaml;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.InputStream;
+import java.lang.reflect.Constructor;
+import java.util.List;
+import java.util.Map;
+
+public class Tools {
+    public static Options createOptionsFromYaml(String fileName) throws FileNotFoundException {
+        Yaml yaml = new Yaml();
+        Map<String,Object> yamlData = yaml.load(new FileReader(new File(fileName)));
+        return new Options(
+                 yamlData.get("host").toString(),
+                (int) yamlData.get("port"),
+                (boolean) yamlData.get("concurMode"),
+                (boolean) yamlData.get("showSendRes"),
+                (Map<String, List<String>>) yamlData.get("clientsMap"));
+    }
+}
